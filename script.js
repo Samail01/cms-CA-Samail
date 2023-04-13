f5017121a490175a140d09b3e1e41c068b7b090b
Samail01 <sapug.2001@hotmail.com>
2023-04-13 23:15:07 +0200

    --->

---
+++ script.js
@@ -0,0 +1,33 @@
+//Øving til CA
+
+
+//Sort()
+
+const fruits = ["Banan", "Orange", "Apple", "Mango"];
+fruits.sort();
+console.log(fruits)
+
+//Ascending
+
+const points = [40, 100, 1, 5, 25, 10];
+points.sort(function(a, b){return a-b});
+console.log(points)
+
+//Descending
+
+const pointss = [40, 100, 1, 5, 25, 10];
+points.sort(function(a, b){return b-a});
+console.log(points)
+
+
+
+
+
+
+
+
+
+
+
+
+//Filter()
